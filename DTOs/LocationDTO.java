94055ce5b7bdfec530b6f3b731b93ef8029f06fa
Teo <teo@journeyxp.com>
2016-09-28 14:16:05 +0200

    Added DTOs and error transfer object.

---
+++ DTOs/LocationDTO.java
@@ -0,0 +1,27 @@
+
+public class LocationDTO {
+	private int locationId;
+	private String name;
+
+	public LocationDTO(int locationId, String name) {
+		super();
+		this.locationId = locationId;
+		this.name = name;
+	}
+
+	public void setLocationId(int locationId) {
+		this.locationId = locationId;
+	}
+
+	public int getLocationId() {
+		return locationId;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
